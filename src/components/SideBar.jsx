33bf42ea503f4a3de796c7efd826e8e67283fd77
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-04 07:05:45 +0300

    Initiate build of desktop Sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import desktopSidebar from '../assets/images/desktop-sidebar.svg'
+
+const SideBar = () => {
+    return (
+        <div className="bg-image w-25 h-75" style={{ backgroundImage: `url(${desktopSidebar})`}} >
+            
+        </div>
+    )
+}
+
+export default SideBar
