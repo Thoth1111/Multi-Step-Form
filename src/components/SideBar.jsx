c3f4209a01c7cf94ff75a40066be9b4274f834b0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-05 09:17:29 +0300

    Create general layout of sidebar for desktop

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,44 @@
 import React from "react";
-import desktopSidebar from '../assets/images/desktop-sidebar.svg'
 
 const SideBar = () => {
     return (
-        <div className="bg-image w-25 h-75" style={{ backgroundImage: `url(${desktopSidebar})`}} >
-            
+        <div className="side-bar">
+            <div className="d-flex gap-3 mx-5 my-2">
+                <div className="flex-item">
+                    <h3>1</h3>  
+                </div>
+                <div className="flex-item">
+                    <p>STEP 1</p>
+                    <p>YOUR INFO</p>
+                </div>                
+            </div>
+            <div className="d-flex gap-3 mx-5 my-2">
+                <div className="flex-item">
+                    <h3>2</h3>  
+                </div>
+                <div className="flex-item">
+                    <p>STEP 2</p>
+                    <p>SELECT PLAN</p>
+                </div>                
+            </div>
+            <div className="d-flex gap-3 mx-5 my-2">
+                <div className="flex-item">
+                    <h3>3</h3>  
+                </div>
+                <div className="flex-item">
+                    <p>STEP 3</p>
+                    <p>ADD-ONS</p>
+                </div>                
+            </div>
+            <div className="d-flex gap-3 mx-5 my-2">
+                <div className="flex-item">
+                    <h3>4</h3>  
+                </div>
+                <div className="flex-item">
+                    <p>STEP 4</p>
+                    <p>SUMMARY</p>
+                </div>                
+            </div>            
         </div>
     )
 }
