443fd45f9bb94ed66d7ddc8ed744f1afb5cd6592
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-07 10:32:35 +0300

    Add main form component

---
+++ src/components/StepForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import SideBar from './SideBar';
+
+const StepForm = () => {
+    const [activeStep, setActiveStep] = useState(1);
+    const totalSteps = 4;
+
+    const handleNext = () => {
+        if (activeStep < totalSteps) {
+            setActiveStep(activeStep + 1);
+        }
+    };
+
+    const handleBack = () => {
+        if (activeStep > 1) {
+            setActiveStep(activeStep - 1);
+        }
+    };
+
+    return (
+        <div>
+            <SideBar activeStep={activeStep} />
+            <div>
+                {activeStep === 1 && <Form1 />}
+                {activeStep === 2 && <Form2 />}
+                {activeStep === 3 && <Form3 />}
+                {activeStep === 4 && <Form4 />}
+                <div>
+                    <button onClick={handleBack}>Go Back</button>
+                    <button onClick={handleNext}>Next Step</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StepForm;
