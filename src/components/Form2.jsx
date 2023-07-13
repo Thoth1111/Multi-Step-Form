3a6f12f96b9badba9cecd0bd6c9fc6ad4bba01e6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-13 09:18:50 +0300

    Complete visual functionality of Form2

--- src/components/Form2.jsx
+++ src/components/Form2.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import arcadeIcon from '../assets/images/icon-arcade.svg';
 import advancedIcon from '../assets/images/icon-advanced.svg';
 import proIcon from '../assets/images/icon-pro.svg';
@@ -8,7 +8,19 @@ const Form2 = () => {
         {planNumber: 1, type: 'Arcade', price: 9, icon: arcadeIcon},
         {planNumber: 2, type: 'Advanced', price: 12, icon: advancedIcon},
         {planNumber: 3, type: 'Pro', price: 15, icon: proIcon}
-    ]
+    ];
+
+    const [yearly, setYearly] = useState(false);
+    const [selected, setSelected] = useState(null);
+
+    const handleToggle = () => {
+        setYearly(!yearly);
+    };
+
+    const handleSelected = (num) => {
+        setSelected(num);
+    }
+
     return (
         <div className="p-5">
             <h1 className="fw-bold">Select your plan</h1>
@@ -16,37 +28,38 @@ const Form2 = () => {
 
             <div className="d-flex gap-4 mb-5" style={{marginTop: '80px'}}>
                 {plans.map((plan) => (
-                    <div className="rounded plans p-4" key={plan.planNumber} style={{border: '1px solid #c9c9c9'}}>
+                    <div 
+                     key={plan.planNumber}
+                     className={`plans rounded p-4 ${plan.planNumber === selected ? 'selected-plan' : ''}`}
+                     onClick={() => handleSelected(plan.planNumber)}
+                     role="button"
+                     tabIndex={0}
+                    >
                         <img src={plan.icon} className="mb-5" alt="arcade icon" />
                         <p className="mt-3 plan-font fw-bold">{plan.type}</p>
-                        <p className="form-font">${plan.price}/mo</p>
+                        { yearly ? (
+                            <>
+                                <p className="form-font">${plan.price * 10}/yr</p>
+                                <p className="form-font" style={{color: '#032b5f', fontSize: '20px'}}>2 months free</p>
+                            </>
+                        ) : (
+                            <p className="form-font">${plan.price}/mo</p>
+                        )}
                     </div>
                 ))}
             </div>
             <div className="d-flex rounded justify-content-center gap-5 pt-2 mt-5 align-items-center" style={{width: '710px', backgroundColor: '#f8f9fe'}}>
-                <p className="form-font">Monthly</p>
+                <p className={`form-font ${yearly ? "" : "active-term"}`}>Monthly</p>
                 <div className="switch-container">
-                    <input className="toggle-checkbox" type="checkbox" id="toggle-switch"/>
-                    <label for="toggle-switch" className="toggler">
+                    <input className="toggle-checkbox" type="checkbox" id="toggle-switch" checked={yearly} onChange={handleToggle}/>
+                    <label htmlFor="toggle-switch" className="toggler">
                         <div className="toggle-circle"></div>
                     </label>
                 </div>
-                <p className="form-font">Yearly</p>                
+                <p className={`form-font ${yearly ? "active-term" : ""}`}>Yearly</p>                
             </div>
         </div>
     );
 };
 
-{/* <div className="form-check form-switch">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        id="toggleSwitch"
-        checked={isToggled}
-        onChange={handleToggle}
-      />
-      <label className="form-check-label" htmlFor="toggleSwitch">
-        Toggle
-      </label>
-    </div> */}
 export default Form2;
