f6d3a6db0b886fb11384e176be7365071cd7d89f
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-08-07 15:01:40 +0300

    Modify Form4 for styling

--- src/components/Form4.jsx
+++ src/components/Form4.jsx
@@ -15,50 +15,50 @@ const Form4 = ( {changePlan} ) => {
 
     return (
         <div className="p-5">
-            <h1 className="fw-bold">Finishing up</h1>
+            <h3 className="fw-bold">Finishing up</h3>
             <p className="form-font mb-5">Double-check everything looks OK before confirming</p>
 
             <div className="rounded p-4" style={{backgroundColor: '#f8f9fe'}}>
                 { yearly ? (
                     <div className="d-flex justify-content-between align-items-center">
                         <div>
-                            <p className="plan-font fw-bold" style={{fontSize: '26px'}}>{plan.type} (Yearly)</p>
-                            <a href="#" onClick={handleChangeClick} className="form-font change-link" style={{fontSize: '22px'}}>change</a>
+                            <p className="plan-font fw-bold">{plan.type} (Yearly)</p>
+                            <a href="#" onClick={handleChangeClick} className="form-font change-link">change</a>
                         </div>
                         <div>
-                            <p className="plan-font fw-bold" style={{fontSize: '26px'}}>${plan.price}/yr</p>
+                            <p className="plan-font fw-bold">${plan.price}/yr</p>
                         </div>
                     </div>
                 ) 
                 : (
                     <div className="d-flex justify-content-between align-items-center">
                         <div>
-                            <p className="plan-font fw-bold" style={{fontSize: '26px'}}>{plan.type} (Monthly)</p>
-                            <a href="#" onClick={handleChangeClick} className="form-font change-link" style={{fontSize: '22px'}}>change</a>
+                            <p className="plan-font fw-bold">{plan.type} (Monthly)</p>
+                            <a href="#" onClick={handleChangeClick} className="form-font change-link">change</a>
                         </div>
                         <div>
-                            <p className="plan-font fw-bold" style={{fontSize: '26px'}}>${plan.price}/mo</p>
+                            <p className="plan-font fw-bold">${plan.price}/mo</p>
                         </div>
                     </div>
                 )}
                 <hr className="mx-1" style={{color: '#c9c9c9'}}/>
                 {addOns.map((addOn) => (
                     <div className="d-flex justify-content-between">
-                        <p className="form-font" style={{fontSize: '22px'}}>{addOn.name}</p>
-                        { yearly ? <p className="form-font" style={{color: 'black', fontSize: '22px'}}>+${addOn.price}/yr</p> 
-                        : <p className="form-font" style={{color: 'black', fontSize: '22px'}}>+${addOn.price}/mo</p> }
+                        <p className="form-font">{addOn.name}</p>
+                        { yearly ? <p className="form-font" style={{color: 'black'}}>+${addOn.price}/yr</p> 
+                        : <p className="form-font" style={{color: 'black'}}>+${addOn.price}/mo</p> }
                     </div>
                 ))}
             </div>            
             { yearly ? (
                 <div className="d-flex justify-content-between p-4">
-                    <p className="form-font" style={{fontSize: '22px'}}>Total (per year)</p> 
-                    <p className="form-font fw-bold" style={{color: '#4a3ee1', fontSize: '26px'}}>${totalPrice}/yr</p>
+                    <p className="form-font">Total (per year)</p> 
+                    <p className="form-font fw-bold" style={{color: '#4a3ee1'}}>${totalPrice}/yr</p>
                 </div>
             ) : (
                 <div className="d-flex justify-content-between p-4">
-                    <p className="form-font" style={{fontSize: '22px'}}>Total (per month)</p>
-                    <p className="form-font fw-bold" style={{color: '#4a3ee1', fontSize: '26px'}}>${totalPrice}/mo</p>
+                    <p className="form-font">Total (per month)</p>
+                    <p className="form-font fw-bold" style={{color: '#4a3ee1'}}>${totalPrice}/mo</p>
                 </div>
             )}
         </div>
